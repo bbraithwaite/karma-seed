075581afa2b6049fc905f08a0ec562c1bf20b90c
bbraithwaite <bradley.braithwaite@gmail.com>
2015-02-26 22:56:27 +0100

    removed jasmine dep from config

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'chai', 'fixture'],
-    // or can be: ['mocha', 'chai'],
+    // or can be: ['jasmine'],
 
     // list of files / patterns to load in the browser
     files: [
